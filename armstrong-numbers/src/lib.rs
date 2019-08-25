d4da5d91d591840b788daf90e11fc1e2ecd44dbe
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 13:20:41 -0400

    get digits and calc armstrong

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,12 @@
+fn digits(n: u32) -> Vec<u32> {
+    match n {
+        0 => vec![],
+        x => [digits(x / 10), vec![x % 10]].concat()
+    }
+}
+
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let d = digits(num);
+    let n = d.len() as u32;
+    d.iter().fold(0, |acum, x| acum + x.pow(n)) == num
 }
