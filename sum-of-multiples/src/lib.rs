31cbc4d98e89ad49faee098dd79ad0b9690e3cf2
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 10:57:44 -0400

    a complete solution, but for 3 or more factors uses iterators

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,9 @@
+// Sum of multiples
+// Author: Eduardo Díaz
+// Uses math formulas to speed up calculations
+
+
+// create pairs from a vector
 fn pairs<I: IntoIterator>(x: I) -> impl Iterator<Item = (I::Item, I::Item)>
 where
     I::Item: Clone,
@@ -8,6 +14,8 @@ where
         .flat_map(move |t| std::iter::repeat(t.1).zip(x.into_iter().skip(t.0 + 1)))
 }
 
+
+// greatest common divisor of 2 numbers
 fn gcd(a: &u32, b: &u32) -> u32 {
     if *a == 0 || *b == 0 {
         0
@@ -20,6 +28,7 @@ fn gcd(a: &u32, b: &u32) -> u32 {
     }
 }
 
+// less commons multiple of 2 numbers
 fn lcm(a: &u32, b: &u32) -> u32 {
     if *a == 0 || *b == 0 {
         0
@@ -28,6 +37,7 @@ fn lcm(a: &u32, b: &u32) -> u32 {
     }
 }
 
+// gauss formula for sum
 fn sum_mult(n: u32, limit: u32) -> u32 {
     if n == 0 {
         0
@@ -38,25 +48,38 @@ fn sum_mult(n: u32, limit: u32) -> u32 {
 }
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let lcms: Vec<u32> = pairs(factors)
-        .collect::<Vec<_>>()
-        .iter()
-        .map(|w| lcm(w.0, w.1))
-        .collect();
+    if factors.len() > 3 {
+        // this is clasical solution, iterating over all numbers until limit
+        // but a faster solution will use gauss formula (see else)
+        // with more time will improve this and not else case will be needed
+        (0..limit).filter(|x| factors.iter().fold(false, |r, f| r || x % f == 0 )).sum()
+    }
+    else {
+        // uses math to solve with few iterations
+        // uses gauss formula to sum all factors
+        let sum = factors.iter().fold(0, |acum, f| acum + sum_mult(*f, limit));
 
-    let sum = factors.iter().fold(0, |acum, f| acum + sum_mult(*f, limit));
-    let neg = lcms.iter().fold(0, |neg, n| neg + sum_mult(*n, limit));
+        // we must substract all lcms of factors
+        let lcms: Vec<u32> = pairs(factors)
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|w| lcm(w.0, w.1))
+            .collect();
+        let neg = lcms.iter().fold(0, |neg, n| neg + sum_mult(*n, limit));
 
-    let mut add_lcms: Vec<u32> = pairs(&lcms)
-        .collect::<Vec<_>>()
-        .iter()
-        .map(|w| lcm(w.0, w.1))
-        .collect();
-    add_lcms.sort();
-    add_lcms.dedup();
-    let pos = add_lcms
-        .iter()
-        .fold(0, |acum, n| acum + sum_mult(*n, limit));
+        // but if we have coprime factors we must correct
+        let mut add_lcms: Vec<u32> = pairs(&lcms)
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|w| lcm(w.0, w.1))
+            .collect();
+        add_lcms.sort();
+        add_lcms.dedup();
+        let pos = add_lcms
+            .iter()
+            .fold(0, |acum, n| acum + sum_mult(*n, limit));
 
-    sum - neg + pos
+        // finally the result    
+        sum - neg + pos
+    } 
 }
