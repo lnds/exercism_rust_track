29e236ce9a94088ead8aa0bbeddfddc002f64b86
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 12:22:03 -0400

    implementación imperativa

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut result = vec![];
+    let mut num = n;
+    let mut candidate = 2;
+    while num > 1 {
+        while num % candidate == 0 {
+            result.push(candidate);
+            num /= candidate;
+        }
+        candidate += 1;
+    }
+    if num > 1 {
+        result.push(num)
+    }
+    result
 }
