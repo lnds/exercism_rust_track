0ef7b1a4c87b52f54b660c76e20e1e473f15c63a
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 11:59:30 -0400

    beer-song exercise

---
+++ beer-song/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn verse(n: i32) -> String {
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"),
+        3 ..= 99 => format!("{b} bottles of beer on the wall, {b} bottles of beer.\nTake one down and pass it around, {c} bottles of beer on the wall.\n", b=n, c=n-1),
+        _ => String::new()
+    }
+}
+
+pub fn sing(start: i32, end: i32) -> String {
+    (end..=start).rev().map(|n| verse(n)).collect::<Vec<String>>().join("\n")
+}
