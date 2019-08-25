3577ddcdddd011c0a3ca710709f0f9c7ae9fb0b6
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 12:00:19 -0400

    nth-prime exercise

---
+++ nth-prime/src/lib.rs
@@ -0,0 +1,48 @@
+
+fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        false
+    } 
+    else if n < 4 {
+        true
+    } 
+    else if n % 2 == 0 {
+        false
+    }
+    else if n < 9 {
+        true
+    }
+    else if n % 3 == 0 {
+        false
+    }
+    else {
+        let nf = n as f64;
+        let r = nf.sqrt().floor() as u32;
+        let mut f = 5;
+        while f <= r {
+            if n % f == 0 {
+                return false;
+            } 
+            if n % (f+2) == 0 {
+                return false;
+            }
+            f = f + 6;
+        }
+        true
+    }
+}
+
+pub fn nth(n: u32) -> u32 {
+    let mut count = 0;
+    let mut candidate : u32 = 1;
+    loop {
+        candidate += 1;
+        if is_prime(candidate) {
+            count += 1;
+        }
+        if count > n {
+            break;
+        }
+    }
+    candidate
+}
