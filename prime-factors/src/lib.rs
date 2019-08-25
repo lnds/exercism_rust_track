431e741120f649b13f8069334121830fc987a293
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 12:35:00 -0400

    versión más concisa de factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -2,15 +2,12 @@ pub fn factors(n: u64) -> Vec<u64> {
     let mut result = vec![];
     let mut num = n;
     let mut candidate = 2;
-    while num > 1 {
+    while candidate <= num && num > 1 {
         while num % candidate == 0 {
             result.push(candidate);
             num /= candidate;
-        }
+        } 
         candidate += 1;
-    }
-    if num > 1 {
-        result.push(num)
     }
     result
 }
