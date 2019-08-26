02f682964f9c1b6c7b9eaf180ae751d1684f8b7b
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 20:07:49 -0400

    solve bob

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,20 @@
+fn ask(message: &str) -> bool {
+    message.ends_with('?')
+}
+
+fn yell(message: &str) -> bool {
+    let a = message.chars().filter(|c|c.is_alphabetic()).count();
+    let b = message.chars().filter(|c|c.is_uppercase()).count();
+    a > 0 && b > 0 && a == b
+}
+
+
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    match message.trim() {
+        msg if ask(msg) && yell(msg) => "Calm down, I know what I\'m doing!",
+        msg if ask(msg) => "Sure.",
+        msg if yell(msg) => "Whoa, chill out!",
+        msg if msg.is_empty() => "Fine. Be that way!",
+        _ => "Whatever."
+    }
 }
