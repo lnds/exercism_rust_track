2e59fe40c35fd73d302ef0b5c25f29699a2f461e
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-26 15:17:54 -0400

    implements diffie hellman

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,21 +1,29 @@
+fn modular_pow(base: u64, exponent: u64, modulus: u64) -> u64 {
+    let mut c: u64 = 1;
+    let mut e = exponent;
+    let mut b = base % modulus;
+    loop {
+        if e % 2 == 1 {
+            c *= b;
+            c %= modulus;
+        }
+        if e == 1 {
+            return c;
+        }
+        e /= 2;
+        b *= b;
+        b %= modulus;
+    }
+}
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    (1 + p) / 2 + 1
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+    modular_pow(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+    modular_pow(b_pub, a, p)
 }
