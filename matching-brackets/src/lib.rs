1722c6bcb9b8e03312fa9a3939bdebfab5e84eb6
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 20:31:14 -0400

    simple solution

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,34 +1,12 @@
-fn is_open_bracket(c: char) -> bool {
-    c == '(' || c == '[' || c == '{'
-}
-
-fn is_close_bracket(c: char) -> bool {
-    c == ')' || c == ']' || c == '}'
-}
-
-fn match_bracket(c: char, top: Option<&char>) -> bool {
-    match top {
-        None => false,
-        Some(b) => match b {
-            '(' => c == ')',
-            '[' => c == ']',
-            '{' => c == '}',
-            _ => false,
-        },
-    }
-}
-
 pub fn brackets_are_balanced(string: &str) -> bool {
     let mut stack: Vec<char> = vec![];
     for c in string.chars() {
-        if is_open_bracket(c) {
-            stack.push(c)
-        } else if is_close_bracket(c) {
-            if !match_bracket(c, stack.last()) {
-                return false;
-            } else {
-                stack.pop();
-            }
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' => if stack.pop() != Some('(') { return false },
+            ']' => if stack.pop() != Some('[') { return false },
+            '}' => if stack.pop() != Some('{') { return false },
+            _ => ()
         }
     }
     stack.is_empty()
