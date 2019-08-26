cbae9240f2b39fe26b64b7a8acf385a92fdf11e7
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 21:05:00 -0400

    solve collatz

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,18 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    match n {
+        0 => None,
+        _ =>  {
+            let mut c = n;
+            let mut count = 0;
+            while c > 1 {
+                if c % 2 == 0 {
+                    c /= 2
+                } else {
+                    c = c * 3 + 1
+                }
+                count += 1;
+            }
+            Some(count)
+        }
+    }
 }
