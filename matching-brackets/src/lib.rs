799ea77d0f986f359e79e172dce0239a30762982
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-25 20:22:06 -0400

    solve matching-brackets

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,35 @@
+fn is_open_bracket(c: char) -> bool {
+    c == '(' || c == '[' || c == '{'
+}
+
+fn is_close_bracket(c: char) -> bool {
+    c == ')' || c == ']' || c == '}'
+}
+
+fn match_bracket(c: char, top: Option<&char>) -> bool {
+    match top {
+        None => false,
+        Some(b) => match b {
+            '(' => c == ')',
+            '[' => c == ']',
+            '{' => c == '}',
+            _ => false,
+        },
+    }
+}
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut stack: Vec<char> = vec![];
+    for c in string.chars() {
+        if is_open_bracket(c) {
+            stack.push(c)
+        } else if is_close_bracket(c) {
+            if !match_bracket(c, stack.last()) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+    }
+    stack.is_empty()
 }
