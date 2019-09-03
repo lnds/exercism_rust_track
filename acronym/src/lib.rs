ce5c1d0ada3d9dd2d8c4c073506470616fcbed2b
Eduardo Díaz <ediaz@dcc8090.cl>
2019-09-02 22:53:55 -0400

    better solution

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -2,7 +2,12 @@ pub fn abbreviate(phrase: &str) -> String {
     phrase
         .split(|c: char| c.is_whitespace() || c == '-')
         .flat_map(|w| {
-            let r = w.chars().skip_while(|c| c.is_uppercase()).filter(|c| c.is_uppercase());
-            w.chars().take(1).chain(r)
-        }).flat_map(char::to_uppercase).collect()
+            w.chars().take(1).chain(
+                w.chars()
+                    .skip_while(|c| c.is_uppercase())
+                    .filter(|c| c.is_uppercase()),
+            )
+        })
+        .flat_map(char::to_uppercase)
+        .collect()
 }
