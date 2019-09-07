1a3c23d0b1c9825f8ef6d26cbbd3485513b8ec2e
Eduardo Díaz <ediaz@dcc8090.cl>
2019-09-07 14:18:07 -0400

    format

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let limit = upper_bound as usize;
-    let mut sieve = vec![true; limit+1];
+    let mut sieve = vec![true; limit + 1];
     for i in 2..=limit {
-        for j in (i+i..=limit).step_by(i) {
+        for j in (i + i..=limit).step_by(i) {
             sieve[j] = false;
         }
     }
