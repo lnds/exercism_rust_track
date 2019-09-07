2f6fbd17e52c1646aa5dd2a635dadcd78fd20b1d
Eduardo Díaz <ediaz@dcc8090.cl>
2019-09-07 16:49:10 -0400

    better solution to triangles

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,74 +1,42 @@
 use std::cmp::PartialOrd;
 use std::ops::Add;
 
-enum TriangleType<T> {
-    Equilateral(T),
-    Isosceles(T, T),
-    Scalene(T, T, T),
+#[derive(PartialEq)]
+enum Kind {
+    Equilateral,
+    Isosceles,
+    Scalene,
 }
 
-use TriangleType::{Equilateral, Isosceles, Scalene};
+use Kind::{Equilateral, Isosceles, Scalene};
 
-pub struct Triangle<T> {
-    triangle: TriangleType<T>,
-}
+pub struct Triangle(Kind);
 
-impl<T: PartialEq + PartialOrd + Add<Output = T> + Copy + Default> Triangle<T> {
-    pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
-        let a: T = sides[0];
-        let b: T = sides[1];
-        let c: T = sides[2];
-        if a == T::default()
-            || b == T::default()
-            || c == T::default()
-            || (a + b) <= c
-            || a + c <= b
-            || b + c <= a
-        {
-            None
-        } else {
-            Some(if a == b && b == c {
-                Triangle {
-                    triangle: Equilateral(a),
-                }
-            } else if a == b && a != c {
-                Triangle {
-                    triangle: Isosceles(a, c),
-                }
-            } else if a == c && a != b {
-                Triangle {
-                    triangle: Isosceles(a, b),
-                }
-            } else if b == c && b != a {
-                Triangle {
-                    triangle: Isosceles(b, a),
-                }
-            } else {
-                Triangle {
-                    triangle: Scalene(a, b, c),
-                }
-            })
+impl Triangle {
+    pub fn build<T>(sides: [T; 3]) -> Option<Triangle>
+    where
+        T: Copy + PartialOrd + Add<Output = T> + Default,
+    {
+        match (sides[0], sides[1], sides[2]) {
+            (a, b, c) if a == T::default() || b == T::default() || c == T::default() => None,
+            (a, b, c) if (a + b) <= c || a + c <= b || b + c <= a => None,
+            (a, b, c) if a == b && b == c => Some(Triangle(Equilateral)),
+            (a, b, c) if a == b && a != c || a == c && a != b || b == c && b != a => {
+                Some(Triangle(Isosceles))
+            }
+            _ => Some(Triangle(Scalene)),
         }
     }
 
     pub fn is_equilateral(&self) -> bool {
-        match self.triangle {
-            Equilateral(_) => true,
-            _ => false,
-        }
+        self.0 == Equilateral
     }
 
     pub fn is_scalene(&self) -> bool {
-        match self.triangle {
-            Scalene(_, _, _) => true,
-            _ => false,
-        }
+        self.0 == Scalene
     }
 
     pub fn is_isosceles(&self) -> bool {
-        match self.triangle {
-            Isosceles(_, _) => true,
-            _ => false,
-        }
+        self.0 == Isosceles
     }
 }
