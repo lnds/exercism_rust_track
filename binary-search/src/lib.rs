d2afa76be7aa3cd56c720715c44f013a9c9e2191
Eduardo Díaz <ediaz@dcc8090.cl>
2019-09-07 21:38:22 -0400

    solves binary-search

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,17 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    match array.len() {
+        0 => None,
+        1 if key == array[0] => Some(0),
+        1 => None,
+        n => {
+            let m = n / 2;
+            if array[m] == key {
+                Some(m)
+            } else if array[m] > key {
+                find(&array[..m], key)
+            } else {
+                find(&array[m..], key).map(|k| m + k)
+            }
+        }
+    }
 }
