19d7deeef936cdd84d0de50212ff3e1c41447a2f
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-23 13:20:26 -0300

    solves two bucket

--- two-bucket/src/lib.rs
+++ two-bucket/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 #[derive(PartialEq, Eq, Debug)]
 pub enum Bucket {
     One,
@@ -16,13 +18,97 @@ pub struct BucketStats {
     pub other_bucket: u8,
 }
 
+fn gcd(a: u8, b: u8) -> u8 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn pour(capacity_1: u8, capacity_2: u8, goal: u8) -> (u8, u8, u8) {
+    let mut from = capacity_1;
+    let mut to = 0;
+    let mut step = 1;
+    while from != goal && to != goal {
+        let temp = min(from, capacity_2 - to);
+        to += temp;
+        from -= temp;
+        step += 1;
+        if from == goal || to == goal {
+            break;
+        }
+        if from == 0 {
+            from = capacity_1;
+            step += 1;
+        }
+        if to == capacity_2 {
+            to = 0;
+            step += 1;
+        }
+    }
+    (from, to, step)
+}
+
 /// Solve the bucket problem
 pub fn solve(capacity_1: u8, capacity_2: u8, goal: u8, start_bucket: &Bucket) -> Option<BucketStats> {
-    unimplemented!(
-        "Given one bucket of capacity {}, another of capacity {}, starting with {:?}, find pours to reach {}, or None if impossible",
-        capacity_1,
-        capacity_2,
-        start_bucket,
-        goal,
-    );
+    if goal > capacity_2 || goal % gcd(capacity_1, capacity_2) != 0 {
+        return None;
+    }
+    match start_bucket {
+        Bucket::One if goal == capacity_2 => Some(BucketStats {
+            moves: 2,
+            goal_bucket: Bucket::Two,
+            other_bucket: capacity_1,
+        }),
+        Bucket::One if goal == capacity_1 => Some(BucketStats {
+            moves: 1,
+            goal_bucket: Bucket::One,
+            other_bucket: 0,
+        }),
+        Bucket::Two if goal == capacity_1 => Some(BucketStats {
+            moves: 2,
+            goal_bucket: Bucket::One,
+            other_bucket: capacity_2,
+        }),
+        Bucket::Two if goal == capacity_2 => Some(BucketStats {
+            moves: 1,
+            goal_bucket: Bucket::Two,
+            other_bucket: 0,
+        }),
+        Bucket::One => {
+            let (from, to, steps) = pour(capacity_1, capacity_2, goal);
+            println!(
+                "CAP 1 ({}, {}, {}) -> from: {}, to: {}, step: {}, bucket: {:?}, goal: {}",
+                capacity_1, capacity_2, goal, from, to, steps, start_bucket, goal
+            );
+            Some(BucketStats {
+                moves: steps,
+                goal_bucket: if goal == from {
+                    Bucket::One
+                } else {
+                    Bucket::Two
+                },
+                other_bucket: if goal == from { to } else { from },
+            })
+        }
+        _ => {
+            let (from, to, steps) = pour(capacity_2, capacity_1, goal);
+
+            println!(
+                "CAP 2  ({}, {}, {})  -> from: {}, to: {}, step: {}, bucket: {:?}, goal: {}",
+                capacity_1, capacity_2, goal, from, to, steps, start_bucket, goal
+            );
+
+            Some(BucketStats {
+                moves: steps,
+                goal_bucket: if goal == from {
+                    Bucket::Two
+                } else {
+                    Bucket::One
+                },
+                other_bucket: if goal == from { to } else { from },
+            })
+        }
+    }
 }
