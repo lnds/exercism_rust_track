5a1d12b78e94c56aea26dc45a8cc46cf770fbc83
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-26 15:43:25 -0300

    solve spiral matrix

--- spiral-matrix/src/lib.rs
+++ spiral-matrix/src/lib.rs
@@ -1,37 +1,28 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    let mut result : Vec<Vec<u32>> = vec![];
-    (0..size).for_each(|_| {
-        let mut v = Vec::new();
-        v.resize(size as usize, 0);
-        result.push(v);
-    });
-    let mut val = 1;
+    let mut result = vec![vec![0; size as usize]; size as usize];
+    let mut val = 1..;
     let mut k: usize = 0;
     let mut l: usize = 0;
     let mut n = size as usize;
     let mut m = n;
     while k < m && l < n {
         for i in l..n {
-            result[k][i] = val;
-            val += 1;
+            result[k][i] = val.next().unwrap();
         }
         k += 1;
-        for i in k..m {
-            result[i][n-1] = val;
-            val += 1;   
+        for r in result.iter_mut().take(m).skip(k) {
+            r[n-1] = val.next().unwrap();
         }
         n -= 1;
         if k < m {
-            for i in (l..=n-1).rev() {
-                result[m-1][i] = val;
-                val += 1;
+            for i in (l..n).rev() {
+                result[m-1][i] = val.next().unwrap();
             }
             m -= 1;
         }
         if l < n {
-            for i in (k..=m-1).rev() {
-                result[i][l] = val;
-                val += 1;
+            for i in (k..m).rev() {
+                result[i][l] = val.next().unwrap();
             }
             l += 1;
         }
