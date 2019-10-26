ad0a39dbaab11717cfbaae9e86d8ea93faa1a698
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-26 15:43:37 -0300

    palindrome products

---
+++ palindrome-products/src/lib.rs
@@ -0,0 +1,73 @@
+use std::collections::BTreeSet;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Palindrome {
+    factors: BTreeSet<(u64, u64)>,
+}
+
+impl Palindrome {
+    pub fn new(a: u64, b: u64) -> Palindrome {
+        let mut btree: BTreeSet<(u64, u64)> = BTreeSet::new();
+        btree.insert((a, b));
+        Palindrome { factors: btree }
+    }
+
+    pub fn value(&self) -> u64 {
+        let (x, y) = self.factors.iter().last().unwrap();
+        x * y
+    }
+
+    pub fn insert(&mut self, a: u64, b: u64) {
+        if !self.factors.contains(&(b, a)) {
+            self.factors.insert((a, b));
+        }
+    }
+}
+
+fn is_palindrome(num: u64) -> bool {
+    let mut reversed = 0;
+    let mut n = num;
+    while n > 0 {
+        reversed = 10 * reversed + n % 10;
+        n /= 10;
+    }
+    reversed == num
+}
+
+pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
+    let mut smallest = None;
+    let mut largest = None;
+    for x in min..=max {
+        for y in min..=max {
+            let value = x * y;
+            if is_palindrome(value) {
+                match smallest {
+                    None => smallest = Some(Palindrome::new(x, y)),
+                    Some(ref mut palindrome) => {
+                        if value == palindrome.value() {
+                            palindrome.insert(x, y)
+                        } else if value < palindrome.value() {
+                            smallest = Some(Palindrome::new(x, y))
+                        }
+                    }
+                }
+                match largest {
+                    None => largest = Some(Palindrome::new(x, y)),
+                    Some(ref mut palindrome) => {
+                        if value == palindrome.value() {
+                            palindrome.insert(x, y)
+                        } else if value > palindrome.value() {
+                            largest = Some(Palindrome::new(x, y))
+                        }
+                    }
+                }
+            }
+        }
+    }
+    match (smallest, largest) {
+        (None, None) => None,
+        (None, _) => None,
+        (_, None) => None,
+        (Some(p1), Some(p2)) => Some((p1.clone(), p2.clone())),
+    }
+}
