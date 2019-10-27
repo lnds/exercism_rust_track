6adc8bcdeaa0b3d1eeb72fc2d4fa27cda722e811
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-26 21:44:28 -0300

    simpler solution

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -7,19 +7,12 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
 
     input.iter().enumerate().flat_map(|(i, row)| {
         cols.iter().enumerate().flat_map(move |(j, col)| {
-            let min = col.iter().fold(None, |min, elem| match min {
-                None => Some((elem, j)),
-                Some((m, _)) if elem < m => Some((elem, j)),
-                min => min,
-            });
-            let max = row.iter().fold(None, |max, elem| match max {
-                None => Some((elem, i)),
-                Some((m, _)) if elem > m => Some((elem, i)),
-                max => max,
-            });
-            match (max, min) {
-                (Some((a, r)), Some((b, c))) if *a == *b => Some((r, c)),
-                _ => None,
+            let min = col.iter().min().unwrap();
+            let max = row.iter().max().unwrap();
+            if min == max {
+                Some((i, j))
+            } else {
+                None
             }
         })
     }).collect()
