dda42118ee4f4c8398064470905cbfd6df01db8f
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-27 12:51:34 -0300

    functional solution

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,78 +1,34 @@
-fn enc(current: Option<(&char, usize)>, source: &str, mut acum: String) -> String {
-    if source.len() == 0 {
-        if let Some((ch, len)) = current {
-            if len > 1 {
-                acum.push_str(&len.to_string());
-            }
-            acum.push(*ch);
-        }
-        return acum;
-    }
-    let ch: char = source.chars().next().unwrap();
-    match current {
-        None => enc(Some((&ch, 1)), &source[1..], acum),
-        Some((c, len)) => {
-            if ch == *c {
-                enc(Some((c, len + 1)), &source[1..], acum)
-            } else {
-                if len > 1 {
-                    acum.push_str(&len.to_string());
-                }
-                acum.push(*c);
-                enc(Some((&ch, 1)), &source[1..], acum)
-            }
-        }
-    }
-}
-
 pub fn encode(source: &str) -> String {
-    enc(None, source, String::new())
-    /*
-    let mut result = String::new();
-    let mut current: Option<char> = None;
-    let mut count = 0;
-    for c in source.chars() {
-        match current {
-            None => {
-                current = Some(c);
-                count = 1;
-            }
-            Some(ch) => {
-                if ch == c {
-                    count += 1;
-                } else {
-                    if count > 1 {
-                        result.push_str(&count.to_string());
-                    }
-                    result.push(ch);
-                    count = 1;
-                    current = Some(c);
-                }
+    let r = source.chars().fold(None, |acum, c| match acum {
+        None => Some((c, 1, String::new())),
+        Some((ch, len, s)) => {
+            if c == ch {
+                Some((ch, len + 1, s))
+            } else if len > 1 {
+                Some((c, 1, s + &len.to_string() + &ch.to_string()))
+            } else {
+                Some((c, 1, s + &ch.to_string()))
             }
         }
+    });
+    match r {
+        None => String::new(),
+        Some((ch, 1, result)) => result + &(ch.to_string()),
+        Some((ch, len, result)) => result + &len.to_string() + &(*ch.to_string()),
     }
-    if let Some(ch) = current {
-        if count > 1 {
-            result.push_str(&count.to_string());
-        }
-        result.push(ch);
-    }
-    result
-    */
 }
 
 pub fn decode(source: &str) -> String {
-    let mut result = String::new();
-    let mut len = 0;
-    for c in source.chars() {
-        if c.is_numeric() {
-            len = len * 10 + c.to_digit(10).unwrap();
-        } else if len == 0 {
-            result.push(c);
-        } else {
-            result.push_str(&c.to_string().repeat(len as usize));
-            len = 0;
-        }
-    }
-    result
+    source
+        .chars()
+        .fold((String::new(), 0), |(result, len), c| {
+            if c.is_numeric() {
+                (result, len * 10 + c.to_digit(10).unwrap())
+            } else if len == 0 {
+                (result + &c.to_string(), 0)
+            } else {
+                (result + &c.to_string().repeat(len as usize), 0)
+            }
+        })
+        .0
 }
