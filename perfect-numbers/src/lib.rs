710385ca6626df393b67e7b3f7693800a8607e50
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-27 21:06:49 -0300

    solve perfect numbers

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
@@ -5,6 +7,17 @@ pub enum Classification {
     Deficient,
 }
 
+fn factors_sum(num: u64) -> u64 {
+    (1u64..num).filter(|x| num % x == 0).sum()
+}
+
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    match num {
+        0 => None,
+        _ => match factors_sum(num).cmp(&num) {
+            Ordering::Equal => Some(Classification::Perfect),
+            Ordering::Less => Some(Classification::Deficient),
+            _ => Some(Classification::Abundant),
+        },
+    }
 }
