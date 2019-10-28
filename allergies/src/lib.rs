ca1a782dcc1bba194480d5f992df0f3abf8dc50d
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-27 23:28:02 -0300

    solve allergies

--- allergies/src/lib.rs
+++ allergies/src/lib.rs
@@ -16,44 +16,27 @@ pub enum Allergen {
 
 use Allergen::*;
 
+const ALLERGENS: [Allergen;8] = [
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+];
+
 impl Allergies {
     pub fn new(score: u32) -> Self {
         Allergies { score }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        let val = *allergen as u32;
-        self.score & val == val
-       
+        self.score & (*allergen as u32) != 0
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        let mut result = vec![];
-        
-        if self.score & 1u32 == 1u32 {
-            result.push(Eggs);
-        }
-        if self.score & 2u32 == 2u32 {
-            result.push(Peanuts)
-        }
-        if self.score & 4u32 == 4u32 {
-            result.push(Shellfish)
-        }
-        if self.score & 8u32 == 8u32 {
-            result.push(Strawberries)
-        }
-        if self.score & 16u32 == 16u32 {
-            result.push(Tomatoes)
-        }
-        if self.score & 32u32 == 32u32 {
-            result.push(Chocolate)
-        }
-        if self.score & 64u32 == 64u32 {
-            result.push(Pollen)
-        }
-        if self.score & 128u32 == 128u32 {
-            result.push(Cats)
-        }
-        result
+        ALLERGENS.iter().filter(|a| self.is_allergic_to(a)).cloned().collect()
     }
 }
