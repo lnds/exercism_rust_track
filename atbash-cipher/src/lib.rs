b2bd5b0842c922692f0b02b78e0dbc811c242c41
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-30 20:54:29 -0300

    solves atbash-cipher

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,9 +1,40 @@
+use std::collections::HashMap;
+
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    let alphabet = (b'a'..=b'z').map(char::from);
+    let tebahpla = (b'a'..=b'z').rev().map(char::from);
+    let collate = alphabet.zip(tebahpla).collect::<HashMap<_, _>>();
+    plain
+        .to_lowercase()
+        .chars()
+        .flat_map(|c| {
+            if c.is_ascii_digit() {
+                Some(c)
+            } else {
+                collate.get(&c).cloned()
+            }
+        })
+        .collect::<Vec<char>>()
+        .chunks(5)
+        .map(|s| s.iter().collect())
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    let alphabet = (b'a'..=b'z').map(char::from);
+    let tebahpla = (b'a'..=b'z').rev().map(char::from);
+    let collate = tebahpla.zip(alphabet).collect::<HashMap<_, _>>();
+    cipher
+        .chars()
+        .flat_map(|c| {
+            if c.is_ascii_digit() {
+                Some(c)
+            } else {
+                collate.get(&c).cloned()
+            }
+        })
+        .collect::<String>()
 }
