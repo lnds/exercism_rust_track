5f91d5a0e2cd772be2895fdbbda7c88ea33b4314
Eduardo Díaz <ediaz@dcc8090.cl>
2019-10-30 19:48:28 -0300

    better solution of pig latin

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,44 +1,46 @@
-const VOWELS: [&str; 7] = ["a", "e", "i", "o", "u", "xr", "yr"];
-const CONSONANTS: [&str; 25] = [
-    "b", "ch", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "qu", "r", "sch", "s",
-    "thr", "th", "t", "v", "w", "x", "z",
-];
-const Y: [&str; 5] = ["ya", "ye", "yi", "yo", "yu"];
+fn rule_1(input: &str) -> Option<String> {
+    if ["a", "e", "i", "o", "u", "xr", "yt"].iter().any(|s| input.starts_with(s)){
+        Some(input.to_string() + "ay")
+    } else {
+        None
+    }
+}
+
+fn find_consonant_cluster(input: &str) -> Option<usize> {
+    input.chars().enumerate().find_map(|(i, c)| match c {
+        'a' | 'e' | 'i' | 'o' | 'u' => Some(i),
+        _ => None,
+    })
+}
+
+fn rule_2(input: &str) -> Option<String> {
+    match find_consonant_cluster(input) {
+        None => None,
+        Some(pos) => Some(input[pos..].to_string() + &input[0..pos] + "ay"),
+    }
+}
+
+fn rule_4(input: &str) -> Option<String> {
+    match input.find('y') {
+        None => None,
+        Some(pos) if pos > 0 => Some(input[pos..].to_string() + &input[..pos] + "ay"),
+        _ => None,
+    }
+}
+
+fn rule_3(input: &str) -> Option<String> {
+    match input.find("qu") {
+        None => None,
+        Some(pos) => Some(input[pos + 2..].to_string() + &input[..pos + 2] + "ay"),
+    }
+}
 
 fn translate_word(input: &str) -> String {
-    let mut result = input.to_string();
-    let m = input.to_lowercase();
-    if VOWELS.iter().any(|s| m.starts_with(s)) {
-        result.push_str("ay");
-        return result;
-    }
-    if let Some(pos) = m.find("y") {
-        if pos != input.len() - 1 {
-            if pos > 0 {
-                let sufix = result.get(0..pos).unwrap().to_string() + "ay";
-                result = result.get(pos..).unwrap().to_string() + &sufix;
-            } else {
-                if Y.iter().any(|s| m.starts_with(s)) {
-                    result = result.get(1..).unwrap().to_string() + "yay"
-                } else {
-                    result = result + "ay";
-                }
-            }
-            return result;
-        }
-    }
-    if let Some(prefix) = CONSONANTS.iter().find(|&s| m.starts_with(s)) {
-        let mut pos = prefix.len();
-        let inc = if *prefix == "q" { 1 } else { 2 };
-        if let Some(s) = result.get(0..1 + inc) {
-            if s.ends_with("qu") {
-                pos += inc;
-            }
-        };
-        let sufix = result.get(0..pos).unwrap().to_string() + "ay";
-        result = result.get(pos..).unwrap().to_string() + &sufix;
-    }
-    result
+    rule_1(input)
+        .or_else(|| rule_3(input))
+        .or_else(|| rule_2(input))
+        .or_else(|| rule_4(input))
+        .unwrap_or_else(|| input.to_string())
 }
 
 pub fn translate(input: &str) -> String {
@@ -46,5 +48,6 @@ pub fn translate(input: &str) -> String {
         .split_whitespace()
         .map(|s| translate_word(s))
         .collect::<Vec<String>>()
-        .join(" ").to_string()
+        .join(" ")
+        .to_string()
 }
