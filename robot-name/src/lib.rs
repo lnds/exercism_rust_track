af439cac892430bafce4ec812648a0a431ab38dc
Eduardo Díaz <ediaz@dcc8090.cl>
2019-11-08 19:15:08 -0300

    fmt

--- robot-name/src/lib.rs
+++ robot-name/src/lib.rs
@@ -1,14 +1,16 @@
+use rand::distributions::Uniform;
 use rand::{thread_rng, Rng};
-use rand::distributions::*;
 
+#[derive(Default)]
 pub struct Robot {
     name: String,
 }
 
-
 impl Robot {
     pub fn new() -> Self {
-        Robot{name:random_name(),}
+        Robot {
+            name: random_name(),
+        }
     }
 
     pub fn name(&self) -> &str {
@@ -20,10 +22,13 @@ impl Robot {
     }
 }
 
-
 fn random_name() -> String {
     let rng = thread_rng();
     let alpha = Uniform::new_inclusive(b'A', b'Z');
     let num = Uniform::new_inclusive(b'0', b'9');
-    rng.sample_iter(&alpha).take(2).chain(rng.sample_iter(&num).take(3)).map(char::from).collect()
+    rng.sample_iter(&alpha)
+        .take(2)
+        .chain(rng.sample_iter(&num).take(3))
+        .map(char::from)
+        .collect()
 }
