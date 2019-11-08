ab731ec641944db0b5d5bcb34a5a3a6cdfa3680f
Eduardo Díaz <ediaz@dcc8090.cl>
2019-11-08 14:10:29 -0300

    solve protein-translation

---
+++ protein-translation/src/lib.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+pub struct CodonsInfo<'a> {
+    data: HashMap<&'a str, &'a str>,
+}
+
+impl<'a> CodonsInfo<'a> {
+    pub fn name_for(&self, codon: &str) -> Option<&'a str> {
+        self.data.get(codon).copied()
+    }
+
+    pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
+        rna.chars().collect::<Vec<_>>().chunks(3).map(|chunk|{
+            let key : &str = &chunk.iter().collect::<String>();
+            self.data.get(key).copied()
+        }).take_while(|p|*p!=Some("stop codon")).collect()
+    }
+}
+
+pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
+    CodonsInfo {
+        data: pairs.iter().copied().collect::<HashMap<_, _>>(),
+    }
+}
