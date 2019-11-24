73fb78db871a6026410442944eebfcb36342891f
Eduardo Díaz <ediaz@dcc8090.cl>
2019-11-24 14:40:43 -0300

    solve custom-set

--- custom-set/src/lib.rs
+++ custom-set/src/lib.rs
@@ -1,48 +1,54 @@
-use std::marker::PhantomData;
 
 #[derive(Debug, PartialEq)]
 pub struct CustomSet<T> {
-    // This field is here to make the template compile and not to
-    // complain about unused type parameter 'T'. Once you start
-    // solving the exercise, delete this field and the 'std::marker::PhantomData'
-    // import.
-    phantom: PhantomData<T>,
+    data: Vec<T>,
 }
 
-impl<T> CustomSet<T> {
-    pub fn new(_input: &[T]) -> Self {
-        unimplemented!();
+impl<T: Copy+Ord> CustomSet<T> {
+    pub fn new(input: &[T]) -> Self {
+        let mut data = Vec::from(input);
+        data.sort();
+        data.dedup();
+        CustomSet {
+            data
+        }
     }
 
-    pub fn contains(&self, _element: &T) -> bool {
-        unimplemented!();
+    pub fn contains(&self, element: &T) -> bool {
+        self.data.contains(element)
     }
 
-    pub fn add(&mut self, _element: T) {
-        unimplemented!();
+    pub fn add(&mut self, element: T) {
+        if !self.contains(&element) {
+            self.data.push(element);
+            self.data.sort()
+        }
     }
 
-    pub fn is_subset(&self, _other: &Self) -> bool {
-        unimplemented!();
+    pub fn is_subset(&self, other: &Self) -> bool {
+        self.data.iter().all(|x| other.contains(x))
     }
 
     pub fn is_empty(&self) -> bool {
-        unimplemented!();
+        self.data.is_empty()
     }
 
-    pub fn is_disjoint(&self, _other: &Self) -> bool {
-        unimplemented!();
+    pub fn is_disjoint(&self, other: &Self) -> bool {
+        !self.data.iter().any(|x| other.data.contains(x))
     }
 
-    pub fn intersection(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn intersection(&self, other: &Self) -> Self {
+        let data : Vec<T> = self.data.iter().filter(|e| other.contains(e)).copied().collect();
+        CustomSet::new(&data)
     }
 
-    pub fn difference(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn difference(&self, other: &Self) -> Self {
+        let data : Vec<T> = self.data.iter().filter(|e| !other.contains(e)).copied().collect();
+        CustomSet::new(&data)
     }
 
-    pub fn union(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn union(&self, other: &Self) -> Self {
+        let data : Vec<T> = self.data.iter().chain(other.data.iter()).copied().collect();
+        CustomSet::new(&data)
     }
 }
