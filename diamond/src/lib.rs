fbbe4d0219a617cdf1c205d03847518fc92d0a90
Eduardo Díaz <ediaz@dcc8090.cl>
2019-12-04 21:18:25 -0300

    refactor diamond

--- diamond/src/lib.rs
+++ diamond/src/lib.rs
@@ -1,17 +1,18 @@
 pub fn get_diamond(c: char) -> Vec<String> {
     let width = usize::from(2 * (c as u8 - b'A') + 1);
-    (b'A'..=(c as u8))
+    let ini = b'A';
+    let end = c as u8;
+    (ini..=end)
         .enumerate()
-        .chain((b'A'..(c as u8)).enumerate().rev())
-        .map(|(i, c)| pad_char(char::from(c), i, width))
+        .chain((ini..end).enumerate().rev())
+        .map(|(i, c)| {
+            let ch = char::from(c);
+            if i == 0 {
+                format!("{:^pad$}", ch, pad = width)
+            } else {
+                let s = format!("{}{:pad$}{}", ch, ' ', ch, pad = 2 * i - 1);
+                format!("{:^pad$}", s, pad = width)
+            }
+        })
         .collect()
-}
-
-fn pad_char(c: char, i: usize, width: usize) -> String {
-    if i == 0 {
-        format!("{:^pad$}", c, pad = width)
-    } else {
-        let result = format!("{}{:pad$}{}", c, ' ', c, pad = 2 * i - 1);
-        format!("{:^pad$}", result, pad = width)
-    }
 }
