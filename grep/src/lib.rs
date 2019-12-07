5d7f3a73e6bdc94efecd0b005968560ef41207cd
Eduardo Díaz <ediaz@dcc8090.cl>
2019-12-07 20:27:09 -0300

    shorter code

--- grep/src/lib.rs
+++ grep/src/lib.rs
@@ -1,6 +1,5 @@
 use failure::Error;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs;
 
 #[derive(Debug)]
 pub struct Flags {
@@ -24,53 +23,40 @@ impl Flags {
 }
 
 pub fn grep(pattern: &str, flags: &Flags, files: &[&str]) -> Result<Vec<String>, Error> {
-    let result = files
+    let r = files
         .iter()
-        .map(|file| grep_in_file(pattern, flags, file, files.len() > 1))
+        .map(|file| {
+            fs::read_to_string(file).map(|text| {
+                let matches = text
+                    .lines()
+                    .enumerate()
+                    .filter(|(_, line)| {
+                        if flags.invert_search {
+                            !contains(pattern, line, &flags)
+                        } else {
+                            contains(pattern, line, &flags)
+                        }
+                    })
+                    .map(|(n, line)| format_output(&line, n + 1, &flags, file, files.len() > 1))
+                    .collect::<Vec<_>>();
+                if flags.print_name_of_files && !matches.is_empty() {
+                    vec![file.to_string()]
+                } else {
+                    matches
+                }
+            })
+        })
         .collect::<Result<Vec<_>, _>>()?;
-    Ok(result.into_iter().flatten().collect())
-}
-
-fn grep_in_file(pat: &str, flags: &Flags, path: &str, mult: bool) -> Result<Vec<String>, Error> {
-    let reader = BufReader::new(File::open(path)?);
-    let show_fn = alt_text(mult, format!("{}:", path));
-    let candidates: Vec<String> = reader
-        .lines()
-        .enumerate()
-        .filter_map(move |(n, line)| match line {
-            Ok(ref line) if contains(pat, &line, flags) => Some((n, line.clone())),
-            _ => None,
-        })
-        .map(|(n, line)| {
-            let show_ln = alt_text(flags.print_line_numbers, format!("{}:", n + 1));
-            format!("{}{}{}", show_fn, show_ln, line)
-        })
-        .collect();
-    if flags.print_name_of_files && !candidates.is_empty() {
-        return Ok(vec![path.to_string()]);
-    }
-    Ok(candidates)
+    Ok(r.into_iter().flatten().collect::<Vec<String>>())
 }
 
 fn contains(pattern: &str, line: &str, flags: &Flags) -> bool {
     let pattern = lower(&flags, pattern);
     let line = lower(&flags, line);
-    flip(&flags, pattern_match(&flags, &pattern, &line))
-}
-
-fn pattern_match(flags: &Flags, pattern: &str, line: &str) -> bool {
     if flags.match_entire_lines {
         pattern == line
     } else {
-        line.contains(pattern)
-    }   
-}
-
-fn flip(flags: &Flags, value: bool) -> bool {
-    if flags.invert_search {
-        !value
-    } else {
-        value
+        line.contains(&pattern)
     }
 }
 
@@ -82,10 +68,14 @@ fn lower(flags: &Flags, text: &str) -> String {
     }
 }
 
-fn alt_text(flag: bool, text: String) -> String {
-    if flag {
-        text
-    } else {
-        "".to_string()
+fn format_output(line: &str, n: usize, flags: &Flags, fname: &str, multi: bool) -> String {
+    let mut result = String::new();
+    if multi {
+        result += &format!("{}:", fname);
     }
+    if flags.print_line_numbers {
+        result += &format!("{}:", n);
+    }
+    result += line;
+    result
 }
