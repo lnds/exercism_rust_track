2b8b1c1957f40e6443c3a4b453d49ec02733bcf0
Eduardo Díaz <ediaz@dcc8090.cl>
2019-12-07 19:54:01 -0300

    inner functions

--- grep/src/lib.rs
+++ grep/src/lib.rs
@@ -31,51 +31,61 @@ pub fn grep(pattern: &str, flags: &Flags, files: &[&str]) -> Result<Vec<String>,
     Ok(result.into_iter().flatten().collect())
 }
 
-
 fn grep_in_file(pat: &str, flags: &Flags, path: &str, mult: bool) -> Result<Vec<String>, Error> {
     let reader = BufReader::new(File::open(path)?);
     let show_fn = alt_text(mult, format!("{}:", path));
-    let candidates : Vec<String> = reader
+    let candidates: Vec<String> = reader
         .lines()
         .enumerate()
         .filter_map(move |(n, line)| match line {
             Ok(ref line) if contains(pat, &line, flags) => Some((n, line.clone())),
             _ => None,
-        }).map(|(n, line)| {
+        })
+        .map(|(n, line)| {
             let show_ln = alt_text(flags.print_line_numbers, format!("{}:", n + 1));
             format!("{}{}{}", show_fn, show_ln, line)
         })
         .collect();
-    if flags.print_name_of_files && !candidates.is_empty(){
+    if flags.print_name_of_files && !candidates.is_empty() {
         return Ok(vec![path.to_string()]);
     }
     Ok(candidates)
 }
 
 fn contains(pattern: &str, line: &str, flags: &Flags) -> bool {
-    let pattern = if flags.case_insensitive {
-        pattern.to_lowercase()
-    } else {
-        pattern.to_string()
-    };
-    let line = if flags.case_insensitive {
-        line.to_lowercase()
-    } else {
-        line.to_string()
-    };
+    let pattern = lower(&flags, pattern);
+    let line = lower(&flags, line);
+    flip(&flags, pattern_match(&flags, &pattern, &line))
+}
 
-    let result = if flags.match_entire_lines {
+fn pattern_match(flags: &Flags, pattern: &str, line: &str) -> bool {
+    if flags.match_entire_lines {
         pattern == line
     } else {
-        line.contains(&pattern)
-    };
+        line.contains(pattern)
+    }   
+}
+
+fn flip(flags: &Flags, value: bool) -> bool {
     if flags.invert_search {
-        !result
+        !value
     } else {
-        result
+        value
     }
 }
 
-fn alt_text(flag:bool, text: String) -> String {
-    if flag { text } else { "".to_string()}
+fn lower(flags: &Flags, text: &str) -> String {
+    if flags.case_insensitive {
+        text.to_lowercase()
+    } else {
+        text.to_string()
+    }
+}
+
+fn alt_text(flag: bool, text: String) -> String {
+    if flag {
+        text
+    } else {
+        "".to_string()
+    }
 }
