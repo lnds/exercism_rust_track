89c8b9dd0138fab3270e3f2497f979dc17946907
Eduardo Díaz <ediaz@dcc8090.cl>
2019-12-07 20:41:29 -0300

    solve accumulate

---
+++ accumulate/src/lib.rs
@@ -0,0 +1,11 @@
+/// What should the type of _function be?
+pub fn map<F, T, R>(input: Vec<T>, mut function: F) -> Vec<R>
+where
+    F: FnMut(T) -> R,
+{
+        let mut result = vec![];
+    for i in input {
+        result.push(function(i))
+    }
+    result
+}
