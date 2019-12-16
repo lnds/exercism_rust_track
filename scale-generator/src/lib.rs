cf41618f75cdbd78ff9c732179c09e25a8e09df2
Eduardo Díaz <ediaz@dcc8090.cl>
2019-12-15 22:28:57 -0300

    soilve scale-generator

--- scale-generator/src/lib.rs
+++ scale-generator/src/lib.rs
@@ -1,14 +1,3 @@
-// You should change this.
-//
-// Depending on your implementation, there are a variety of potential errors
-// which might occur. They aren't checked by the test suite in order to
-// allow the greatest freedom of implementation, but real libraries should
-// provide useful, descriptive errors so that downstream code can react
-// appropriately.
-//
-// One common idiom is to define an Error enum which wraps all potential
-// errors. Another common idiom is to use a helper type such as failure::Error
-// which does more or less the same thing but automatically.
 #[derive(Debug)]
 pub enum Error {
     InvalidTonic(String),
@@ -30,16 +19,15 @@ impl Scale {
         let scale = if FLAT.iter().any(|&x| x == tonic) { FLAT_SCALE} else { SHARP_SCALE };
         println!("tonic = {}, scale = {:?}", tonic, scale);
         if let Some(pos) =  scale.iter().position(|&x| x == tonic || x.to_uppercase() == tonic.to_uppercase()) {
-            Scale::new_scale(tonic, intervals, pos, scale)
+            Scale::new_scale(intervals, pos, scale)
         } else {
             Err(Error::InvalidTonic(tonic.to_string()))
         }
     }
 
-    fn new_scale(tonic: &str, intervals: &str, pos: usize, scale: [&str;12]) -> Result<Scale, Error> {
+    fn new_scale(intervals: &str, pos: usize, scale: [&str;12]) -> Result<Scale, Error> {
         let mut notes = vec![];
         let mut p = pos;
-        println!("tonic {}, intervals {}", tonic, intervals);
         notes.push(scale[p].to_string());
         for interval in intervals.chars() {
                 if interval == 'M' {
@@ -51,7 +39,6 @@ impl Scale {
                 } else {
                     return Err(Error::BadInterval(interval))
                 }
-                println!("pos = {}, p = {}, note={}", pos, p, scale[p].to_string());
                 notes.push(scale[p].to_string());
         }
         notes.pop();
