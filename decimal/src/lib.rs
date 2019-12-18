5bce6dd911d755c31197dc73b8025e4e9ab6dd73
Eduardo Díaz <ediaz@dcc8090.cl>
2019-12-17 23:10:54 -0300

    full solution

--- decimal/src/lib.rs
+++ decimal/src/lib.rs
@@ -1,10 +1,10 @@
-use num_bigint::{BigInt, BigUint, Sign};
+use num_bigint::BigInt;
+use num_traits::identities::{One, Zero};
+use num_traits::pow::Pow;
 use std::ops::{Add, Mul, Sub};
 use std::str::FromStr;
-use num_traits::pow::Pow;
 
-/// Type implementing arbitrary-precision decimal arithmetic
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(Debug)]
 pub struct Decimal {
     mantise: BigInt,
     power: u32,
@@ -12,36 +12,64 @@ pub struct Decimal {
 
 impl Decimal {
     pub fn try_from(input: &str) -> Option<Decimal> {
-        println!("try_from({})", input);
+        let neg = input.starts_with('-');
+        let sign = if neg {
+            BigInt::from(-1i32)
+        } else {
+            BigInt::one()
+        };
+        let input = if neg { &input[1..] } else { &input[..] };
         let parts: Vec<&str> = input.split('.').collect();
-        let result = if parts.len() > 2 {
+        if parts.len() > 2 {
             None
         } else if parts.len() == 1 {
-            match BigInt::from_str(parts[0]) {
-                Ok(mantise) => Some(Decimal {
-                    mantise,
-                    power: 0,
-                }),
-                _ => None,
-            }
+            let mantise = BigInt::from_str(parts[0]).ok()?;
+            Some(Decimal {
+                mantise: mantise * sign,
+                power: 0,
+            })
         } else {
-            match BigInt::from_str(parts[0]) {
-                Ok(int_part) => match BigInt::from_str(parts[1]) {
-                    Ok(dec_part) => {
-                        let power = (parts[1].len()) as u32;
-                        let mult = BigInt::from(10u32).pow(&power);
-                        let sign = if input.starts_with("-") { BigInt::from(-1i32) } else { BigInt::from(1i32)};
-                        Some(Decimal {
-                        mantise: int_part*mult*sign + dec_part,
-                        power,
-                    })},
-                    _ => None,
-                },
-                _ => None,
-            }
-        };
-        println!("-> {:?}", result);
-        result
+            let int_part = BigInt::from_str(parts[0]).ok()?;
+            let dec_part = BigInt::from_str(parts[1]).ok()?;
+            let power = (parts[1].len()) as u32;
+            let mult = BigInt::from(10u32).pow(&power);
+            Some(Decimal {
+                mantise: sign * (int_part * mult + dec_part),
+                power,
+            })
+        }
+    }
+}
+
+impl PartialOrd for Decimal {
+    fn partial_cmp(&self, other: &Decimal) -> Option<std::cmp::Ordering> {
+        if self.power == other.power {
+            self.mantise.partial_cmp(&other.mantise)
+        } else if self.power < other.power {
+            let a = &self.mantise * mult(other.power, self.power);
+            let b = &other.mantise;
+            a.partial_cmp(&b)
+        } else {
+            let a = &other.mantise * mult(self.power, other.power);
+            let b = &self.mantise;
+            b.partial_cmp(&a)
+        }
+    }
+}
+
+impl PartialEq for Decimal {
+    fn eq(&self, other: &Decimal) -> bool {
+        if self.power == other.power {
+            self.mantise.eq(&other.mantise)
+        } else if self.power < other.power {
+            let a = &self.mantise * mult(other.power, self.power);
+            let b = &other.mantise;
+            a.eq(&b)
+        } else {
+            let a = &other.mantise * mult(self.power, other.power);
+            let b = &self.mantise;
+            b.eq(&a)
+        }
     }
 }
 
@@ -49,32 +77,23 @@ impl Add for Decimal {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
+        let a = self.mantise;
+        let b = other.mantise;
         if self.power == other.power {
-            let a = self.mantise;
-            let b = other.mantise;
-            let result = a + b;
             Self {
-                mantise: result,
+                mantise: a + b,
                 power: self.power,
             }
         } else if self.power < other.power {
-            let mult = BigInt::from(10u32).pow(other.power-self.power);
-            let a = self.mantise * mult;
-            let b = other.mantise;
-            let result = a + b;
             Self {
-                mantise: result,
+                mantise: a * mult(other.power, self.power) + b,
                 power: other.power,
             }
         } else {
-            let mult = BigInt::from(10u32).pow(self.power-other.power);
-            let a = self.mantise;
-            let b = other.mantise * mult;
-            let result = a + b;
             Self {
-                mantise: result,
+                mantise: a + b * mult(self.power, other.power),
                 power: self.power,
-            } 
+            }
         }
     }
 }
@@ -83,32 +102,23 @@ impl Sub for Decimal {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
+        let a = self.mantise;
+        let b = other.mantise;
         if self.power == other.power {
-            let a = self.mantise;
-            let b = other.mantise;
-            let result = a - b;
             Self {
-                mantise: result,
+                mantise: a - b,
                 power: self.power,
             }
         } else if self.power < other.power {
-            let mult = BigInt::from(10u32).pow(other.power-self.power);
-            let a = self.mantise * mult;
-            let b = other.mantise;
-            let result = a - b;
             Self {
-                mantise: result,
+                mantise: a * mult(other.power, self.power) - b,
                 power: other.power,
             }
         } else {
-            let mult = BigInt::from(10u32).pow(self.power-other.power);
-            let a = self.mantise;
-            let b = other.mantise * mult;
-            let result = a - b;
             Self {
-                mantise: result,
+                mantise: a - b * mult(self.power, other.power),
                 power: self.power,
-            } 
+            }
         }
     }
 }
@@ -117,32 +127,27 @@ impl Mul for Decimal {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
-        if self.power == other.power {
-            let a = self.mantise;
-            let b = other.mantise;
-            let result = a * b;
-            Self {
-                mantise: result,
-                power: self.power * self.power,
-            }
-        } else if self.power < other.power {
-            let mult = BigInt::from(10u32).pow(other.power-self.power);
-            let a = self.mantise * mult;
-            let b = other.mantise;
-            let result = a * b;
-            Self {
-                mantise: result,
-                power: other.power*other.power,
-            }
-        } else {
-            let mult = BigInt::from(10u32).pow(self.power-other.power);
-            let a = self.mantise;
-            let b = other.mantise * mult;
-            let result = a * b;
-            Self {
-                mantise: result,
-                power: self.power*self.power,
-            } 
+        let a = self.mantise;
+        let b = other.mantise;
+        let mut result = &a * &b;
+        let mut power = self.power;
+        if self.power < other.power && self.power > 0 {
+            result *= mult(other.power, self.power);
+            power = other.power;
+        } else if self.power > other.power && other.power > 0 {
+            result *= mult(self.power, other.power);
+            power = self.power;
+        }
+        if &result % BigInt::from(10) == BigInt::zero() {
+            result /= BigInt::from(10);
+        }
+        Self {
+            mantise: result,
+            power,
         }
     }
+}
+
+fn mult(pow1: u32, pow2: u32) -> BigInt {
+    BigInt::from(10u32).pow(pow1 - pow2)
 }
