7effe74f6701216eed0f52352aec1b72b81907c8
Eduardo Díaz <ediaz@dcc8090.cl>
2019-12-25 20:30:33 -0300

    solve circular-buffer

--- circular-buffer/src/lib.rs
+++ circular-buffer/src/lib.rs
@@ -1,10 +1,7 @@
-
 pub struct CircularBuffer<T> {
-    // This field is here to make the template compile and not to
-    // complain about unused type parameter 'T'. Once you start
-    // solving the exercise, delete this field and the 'std::marker::PhantomData'
-    // import.
     buffer: Vec<T>,
+    head: usize,
+    tail: usize,
 }
 
 #[derive(Debug, PartialEq)]
@@ -13,26 +10,48 @@ pub enum Error {
     FullBuffer,
 }
 
-impl<T> CircularBuffer<T> {
+impl<T> CircularBuffer<T>
+where
+    T: Clone + Default + std::fmt::Debug,
+{
     pub fn new(capacity: usize) -> Self {
         Self {
-            buffer: Vec::with_capacity(capacity)
+            buffer: vec![Default::default(); capacity + 1],
+            head: 0,
+            tail: 0,
         }
     }
 
-    pub fn write(&mut self, _element: T) -> Result<(), Error> {
-        unimplemented!("Write the passed element to the CircularBuffer or return FullBuffer error if CircularBuffer is full.");
+    pub fn write(&mut self, element: T) -> Result<(), Error> {
+        let next = (self.tail + 1) % self.buffer.capacity();
+        if next == self.head {
+            return Err(Error::FullBuffer);
+        }
+        self.buffer[self.tail] = element;
+        self.tail = next;
+        Ok(())
     }
 
     pub fn read(&mut self) -> Result<T, Error> {
-        unimplemented!("Read the oldest element from the CircularBuffer or return EmptyBuffer error if CircularBuffer is empty.");
+        if self.head == self.tail {
+            return Err(Error::EmptyBuffer);
+        }
+        let next = (self.head + 1) % self.buffer.capacity();
+        let value = self.buffer[self.head].clone();
+        self.head = next;
+        Ok(value)
     }
 
     pub fn clear(&mut self) {
-        unimplemented!("Clear the CircularBuffer.");
+        self.head = self.tail;
     }
 
-    pub fn overwrite(&mut self, _element: T) {
-        unimplemented!("Write the passed element to the CircularBuffer, overwriting the existing elements if CircularBuffer is full.");
+    #[allow(unused_must_use)] 
+    pub fn overwrite(&mut self, element: T) {
+        let next = (self.tail + 1) % self.buffer.capacity();
+        if next == self.head {
+            self.head = (self.head + 1) % self.buffer.capacity();
+        } 
+        self.write(element);
     }
 }
