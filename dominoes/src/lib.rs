ae3520c8960e6086824ff9cb41643cce3cc39074
Eduardo Díaz <ediaz@dcc8090.cl>
2020-01-04 18:58:07 -0300

    is_cyclic_directed not needed

--- dominoes/src/lib.rs
+++ dominoes/src/lib.rs
@@ -1,63 +1,60 @@
 #[macro_use]
 extern crate itertools;
 use itertools::Itertools;
-use petgraph::algo::{all_simple_paths, is_cyclic_directed};
+use petgraph::algo::{all_simple_paths};
 use petgraph::graph::{DiGraph, NodeIndex};
 
 pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
     println!("input: {:?}", input);
     if input.is_empty() {
-        Some(vec![])
-    } else if input.len() == 1 {
+        return Some(vec![]);
+    }
+    if input.len() == 1 {
         if input[0].0 == input[0].1 {
-            Some(input.to_vec())
-        } else {
-            None
+            return Some(input.to_vec());
         }
-    } else {
-        let dominoes: Vec<Domino> = input
-            .iter()
-            .enumerate()
-            .map(|(i, &p)| Domino::new(i, p))
-            .chain(input.iter().enumerate().map(|(i, &p)| Domino::rev(i, p)))
-            .collect();
+        return None;
+    }
+    let dominoes: Vec<Domino> = input
+        .iter()
+        .enumerate()
+        .map(|(i, &p)| Domino::new(i, p))
+        .chain(input.iter().enumerate().map(|(i, &p)| Domino::rev(i, p)))
+        .collect();
 
-        let mut graph = DiGraph::<Domino, u32>::new();
-        let indices = dominoes
-            .iter()
-            .map(|d| graph.add_node(d.clone())).collect::<Vec<NodeIndex<_>>>();
+    let mut graph = DiGraph::<Domino, u32>::new();
+    let indices = dominoes
+        .iter()
+        .map(|d| graph.add_node(d.clone()))
+        .collect::<Vec<NodeIndex<_>>>();
 
-        for  (&x, &y) in iproduct!(indices.iter(), indices.iter()) {
-            let a = &graph[x].clone();
-            let b = &graph[y];
-            if a != b && a.connect(b) {
-                graph.add_edge(x, y, 1);
+    for (&x, &y) in iproduct!(indices.iter(), indices.iter()) {
+        let a = &graph[x].clone();
+        let b = &graph[y];
+        if a != b && a.connect(b) {
+            graph.add_edge(x, y, 1);
+        }
+    }
+
+    for (&from, &to) in iproduct!(indices.iter(), indices.iter()) {
+        for path in all_simple_paths::<Vec<NodeIndex>, _>(
+            &graph,
+            from,
+            to,
+            input.len() - 2,
+            Some(input.len() - 1),
+        ) {
+            let dom_path: Vec<Domino> = path.iter().map(|&pi| &graph[pi]).cloned().collect();
+            if dom_path.iter().map(|d| d.id).sorted().dedup().count() == input.len() {
+                println!("dom path => {:?}", dom_path);
+                let doms = dom_path.iter().map(|d| d.pair).collect::<Vec<(u8, u8)>>();
+                if doms[0].0 == doms[doms.len() - 1].1 {
+                    return Some(doms);
+                }
             }
         }
-
-        if is_cyclic_directed(&graph) {
-            for (&from, & to) in iproduct!(indices.iter(), indices.iter())   {
-                    for path in all_simple_paths::<Vec<NodeIndex>, _>(
-                        &graph,
-                        from,
-                        to,
-                        input.len() - 2,
-                        Some(input.len() - 1),
-                    ) {
-                        let dom_path: Vec<Domino> =
-                            path.iter().map(|&pi| &graph[pi]).cloned().collect();
-                        if dom_path.iter().map(|d| d.id).sorted().dedup().count() == input.len() {
-                            println!("dom path => {:?}", dom_path);
-                            let doms = dom_path.iter().map(|d| d.pair).collect::<Vec<(u8, u8)>>();
-                            if doms[0].0 == doms[doms.len() - 1].1 {
-                                return Some(doms);
-                            }
-                        }
-                    }
-                }
-        }
-        None
     }
+    None
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
