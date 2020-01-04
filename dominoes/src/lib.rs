456963f683a6e99556bfe5a1def902d5f6a671a2
Eduardo Díaz <ediaz@dcc8090.cl>
2020-01-04 18:55:12 -0300

    clean loops

--- dominoes/src/lib.rs
+++ dominoes/src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate itertools;
 use itertools::Itertools;
 use petgraph::algo::{all_simple_paths, is_cyclic_directed};
 use petgraph::graph::{DiGraph, NodeIndex};
@@ -19,28 +21,22 @@ pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
             .map(|(i, &p)| Domino::new(i, p))
             .chain(input.iter().enumerate().map(|(i, &p)| Domino::rev(i, p)))
             .collect();
-        println!("dominoes = {:?}", dominoes);
 
         let mut graph = DiGraph::<Domino, u32>::new();
-        let indices = dominoes.iter().map(|d|graph.add_node(d.clone())).collect::<Vec<NodeIndex<_>>>();
-        for index in indices.iter() {
-            let domino: &Domino = &graph[*index].clone();
-            for other_index in indices.iter() {
-                let other_domino: &Domino = &graph[*other_index];
-                if *domino != *other_domino {
-                    if domino.connect(&other_domino) {
-                        graph.add_edge(*index, *other_index, 1);
-                    }
-                }
+        let indices = dominoes
+            .iter()
+            .map(|d| graph.add_node(d.clone())).collect::<Vec<NodeIndex<_>>>();
+
+        for  (&x, &y) in iproduct!(indices.iter(), indices.iter()) {
+            let a = &graph[x].clone();
+            let b = &graph[y];
+            if a != b && a.connect(b) {
+                graph.add_edge(x, y, 1);
             }
         }
 
         if is_cyclic_directed(&graph) {
-            for &from in indices.iter() {
-                for &to in indices.iter() {
-                    if from == to {
-                        continue;
-                    }
+            for (&from, & to) in iproduct!(indices.iter(), indices.iter())   {
                     for path in all_simple_paths::<Vec<NodeIndex>, _>(
                         &graph,
                         from,
@@ -52,32 +48,34 @@ pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
                             path.iter().map(|&pi| &graph[pi]).cloned().collect();
                         if dom_path.iter().map(|d| d.id).sorted().dedup().count() == input.len() {
                             println!("dom path => {:?}", dom_path);
-                            let doms = dom_path
-                                .iter()
-                                .map(|d| d.pair)
-                                .collect::<Vec<(u8, u8)>>();
+                            let doms = dom_path.iter().map(|d| d.pair).collect::<Vec<(u8, u8)>>();
                             if doms[0].0 == doms[doms.len() - 1].1 {
                                 return Some(doms);
                             }
                         }
                     }
                 }
-            }
         }
         None
     }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
-struct Domino{id:usize, pair:(u8, u8)}
+struct Domino {
+    id: usize, // because could use multiple domino sets
+    pair: (u8, u8),
+}
 
 impl Domino {
     fn new(id: usize, pair: (u8, u8)) -> Self {
-        Domino{id, pair}
+        Domino { id, pair }
     }
 
     fn rev(id: usize, pair: (u8, u8)) -> Self {
-        Domino{id, pair: (pair.1, pair.0)}
+        Domino {
+            id,
+            pair: (pair.1, pair.0),
+        }
     }
 
     fn connect(&self, other: &Domino) -> bool {
